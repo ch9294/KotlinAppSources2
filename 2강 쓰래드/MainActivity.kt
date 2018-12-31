cd01a216ff021b99f05c178fc9e543a8cb8ed7f1
최천일 <ch9294@naver.com>
2018-12-31 19:56:57 +0900

    Update MainActivity.kt

--- 2강 쓰래드/MainActivity.kt
+++ 2강 쓰래드/MainActivity.kt
@@ -34,10 +34,14 @@ class MainActivity : AppCompatActivity() {
     inner class ThreadClass1 : Thread(){
         override fun run() {
             while(isRunning){
+                // 안드로이드에서는 Thread.sleep() 대신에 SystemClock.sleep()을 사용한다.
                 SystemClock.sleep(100)
                 var now = System.currentTimeMillis()
                 Log.d("test1", "쓰래드 : ${now}")
 
+                // 오레오(8.0)부터 사용자 정의 쓰레드에서 화면 처리가 가능하게 되었다.
+                // 하지만 전 세계 안드로이드 버전의 대부분은 8.0 미만의 경우가 95%가 되기 때문에
+                // 이 방법은 거의 사용하지 않는 것이 좋다.
                 textView2.text = "쓰래드 : ${now}"
             }
         }
